18e36790ae93ee8090c407cf1238dfc470dd17df
joshua9154 <44042301+joshua9154@users.noreply.github.com>
2023-02-21 12:36:48 -0500

    made a database connection

--- db/db.js
+++ db/db.js
@@ -4,9 +4,9 @@ var mysql = require("mysql");
 var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : 'us-east.connect.psdb.cloud',
-  user            : 'w5wf94r18cxf2qucnz7a',
-  password        : 'pscale_pw_UQYzLPCNoNTe4YDn0qjUNWr83tWuwnG8Ckih357oOKT',
-  database        : 'patient-intake',
+  user            : 'evf9sx5vuplnvljp2i65',
+  password        : 'pscale_pw_pyiXZG4J2b6sJF5ad17ExOnjlPkVU8J9LIuaoMjPisr',
+  database        : 'cs_631_project',
   ssl : {
       rejectUnauthorized: true
   }
