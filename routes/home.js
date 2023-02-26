a474919844a2370807dc4f6c6bad8752929640fc
cc862 <cc862@njit.edu>
2023-02-26 12:49:42 -0500

    api testing

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db/db");
 
- var express = require("express");
- var router = express.Router();
- let request = require('request');
- const session = require('express-session');
- const pool = require("../db/db");
- const flash = require('connect-flash');
- 
+const session = require('express-session');
+const flash = require('connect-flash');
 const cors = require('cors');
-
 
 router.use(session({
   secret: 'secret',
@@ -17,72 +14,56 @@ router.use(session({
 
 router.use(flash());
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: 'http://localhost:3000', 
+  credentials: true,            
+  optionSuccessStatus: 200
+};
+
 router.use(cors(corsOptions));
 
+router.get("/", (req, res) => {
+  res.render("home", { title: "home" });
+});
 
-router.get("/", function(req, res, next) {
-   res.render("home", { title: "home" });
- });
- 
-
- 
- router.get("/view", async function(req, res, next) {
-
-  pool.query("select * from Users;" ,(err, rows, fiels) => {  
-      data=JSON.stringify(rows)
-      result= JSON.parse(data)
-     // result=""+body.insertId
+router.get("/view", async (req, res) => {
+  pool.query("SELECT * FROM Users", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      const data = JSON.stringify(rows);
+      const result = JSON.parse(data);
       res.render("subscribed", {
-     title: "Users",
-      result 
-
-       });
-   
-  
- });
-  
- 
-         
-     
+        title: "Users",
+        result
+      });
+    }
+  });
 });
 
 router.get("/data", (req, res) => {
-    pool.query("select * from Users;" ,(err, rows, fiels) => {  
-       result=JSON.stringify(rows)
-       
-       //result= JSON.parse(data)
-    
-    
-    if (!err) {
-        res.send(result)
-        console.log(fiels);
-    } else {
-        res.status(400).send(err)
+  pool.query("SELECT * FROM Users", (err, rows) => {
+    if (err) {
       console.log(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      const result = JSON.stringify(rows);
+      res.send(result);
     }
   });
 });
 
 router.post("/", async (req, res) => {
-  var users= req.body
- 
-   
-    pool.query("INSERT INTO Users (Username,Password) VALUES ('"+users.Username+"','"+users.Password+"');" ,(err, rows, fiels) => {  
- if (!err) {
-   
-   res.json(rows);
-   console.log(fiels);
- } else {
-     res.status(400).send(err)
+  const users = req.body;
+  pool.query("INSERT INTO Users (Username, Password) VALUES ('"+users.Username+"','"+users.Password+"');", (err, rows) => {
+    if (err) {
       console.log(err);
- } });
- 
-  
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.json(rows);
+    }
+  });
 });
- 
+
 module.exports = router;
