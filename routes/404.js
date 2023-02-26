a2a068872be3fd0eee1db6062e6528e23c335d2a
cc862 <cc862@njit.edu>
2023-02-26 12:27:37 -0500

    changed 404 to match home

--- routes/404.js
+++ routes/404.js
@@ -1,44 +1,88 @@
- var express = require("express");
- var router = express.Router();
- let request = require('request');
- const pool = require("../db/db");
- 
- 
+
+var express = require("express");
+var router = express.Router();
+let request = require('request');
+const session = require('express-session');
+const pool = require("../db/db");
+const flash = require('connect-flash');
+
 const cors = require('cors');
 
 
+router.use(session({
+ secret: 'secret',
+ resave: false,
+ saveUninitialized: false
+}));
+
+router.use(flash());
+
 const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+   origin:'http://localhost:3000', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200
 }
 router.use(cors(corsOptions));
 
 
 router.get("/", function(req, res, next) {
-   res.render("404", { title: "404" });
- });
+  res.render("home", { title: "home" });
+});
 
 
- router.get("/view", async function(req, res, next) {
 
-  pool.query("select * from Users;" ,(err, rows, fiels) => {  
-      data=JSON.stringify(rows)
-      result= JSON.parse(data)
-     // result=""+body.insertId
-      res.render("login", {
-     title: "Users",
-      result 
+router.get("/view", async function(req, res, next) {
 
-       });
-   
-  
- });
+ pool.query("select * from Users;" ,(err, rows, fiels) => {  
+     data=JSON.stringify(rows)
+     result= JSON.parse(data)
+    // result=""+body.insertId
+     res.render("subscribed", {
+    title: "Users",
+     result 
+
+      });
   
  
-         
-     
 });
  
+
+        
+    
+});
+
+router.get("/data", (req, res) => {
+   pool.query("select * from Users;" ,(err, rows, fiels) => {  
+      result=JSON.stringify(rows)
+      
+      //result= JSON.parse(data)
+   
+   
+   if (!err) {
+       res.send(result)
+       console.log(fiels);
+   } else {
+       res.status(400).send(err)
+     console.log(err);
+   }
+ });
+});
+
+router.post("/", async (req, res) => {
+ var users= req.body
+
+  
+   pool.query("INSERT INTO Users (Username,Password) VALUES ('"+users.Username+"','"+users.Password+"');" ,(err, rows, fiels) => {  
+if (!err) {
+  
+  res.json(rows);
+  console.log(fiels);
+} else {
+    res.status(400).send(err)
+     console.log(err);
+} });
+
  
+});
+
 module.exports = router;
