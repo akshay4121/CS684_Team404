862a9bf7f2626221bfb9b00d275824e0b4980f26
cc862 <cc862@njit.edu>
2023-02-27 19:32:33 -0500

    added proper status codes for actions conducted by user

--- routes/signup.js
+++ routes/signup.js
@@ -26,6 +26,8 @@ const userExists = async (username, email) => {
   return rows.length > 0;
 };
 
+
+//show signup page with flashes if they exist
 router.get("/", (req, res) => {
   res.render("signup", { title: "Signup", messages: req.flash() });
 });
@@ -36,30 +38,33 @@ router.post("/", async (req, res) => {
   // Check if passwords match
   if (password !== confirmPassword) {
     req.flash('error', 'Passwords do not match');
-    res.redirect("/signup?registrationSuccess=false");
-  } else if (username.slice(0, -1).indexOf(' ') !== -1) {
+    res.status(400).redirect("/signup?registrationSuccess=false"); //query string false annd displays error pass mismatch 
+    //creates newstring of username upto last character excluding the space, 
+    //and checkes if space in string this will tell us if spaces exist in input username
+  } else if (username.slice(0, -1).indexOf(' ') !== -1) { 
     req.flash('error', 'Username cannot contain spaces');
-    res.redirect("/signup?registrationSuccess=false");
+    res.status(400).redirect("/signup?registrationSuccess=false"); //query string false and displays error username can not have spaces 
+    //checks length of username 
   } else if (username.trim().length < 8) {
     req.flash('error', 'Username must be at least 8 characters long');
-    res.redirect("/signup?registrationSuccess=false");
+    res.status(400).redirect("/signup?registrationSuccess=false"); //query string false an displays error username not 8 characters min
   } else {
     // Check if user already exists
     const exists = await userExists(username.trim(), email.trim());
     if (exists) {
       req.flash('error', 'User already exists');
-      res.redirect("/signup?registrationSuccess=false");
+      res.status(409).redirect("/signup?registrationSuccess=false"); //query string false, and displays error user already exits 
     } else {
       try {
         await pool.execute(
           "INSERT INTO Users (Username, Password, Email) VALUES (?, ?, ?)",
-          [username.trim(), password, email.trim()]
+          [username.trim(), password, email.trim()] //trims spaces at end of username and email 
         );
         req.flash('success', 'Registration successful. Please log in.');
-        res.redirect('/signin?registrationSuccess=true');
+        res.status(201).redirect('/signin?registrationSuccess=true'); //query string true, redirects to login with success 
       } catch (err) {
         req.flash('error', err.message);
-        res.redirect("/signup?registrationSuccess=false");
+        res.status(500).redirect("/signup?registrationSuccess=false"); //query string false
         console.log(err);
       }
     }
