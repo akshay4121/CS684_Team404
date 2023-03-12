e6a0d403a86f0c617245f48f8fcd356ebd9773af
cc862 <cc862@njit.edu>
2023-03-12 16:13:11 -0400

    edit on home page to try to work with render take 21

--- routes/home.js
+++ routes/home.js
@@ -2,26 +2,27 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db/db");
 const axios = require("axios");
-const cors = require('cors');
+//const cors = require('cors');
 require('dotenv').config()
 
 
-const session = require('express-session');
-const flash = require('connect-flash');
+//const session = require('express-session');
+//const flash = require('connect-flash');
 
-const API_KEY = process.env.API_KEY;
+var API_KEY = process.env.API_KEY;
 //render debug to see key in log
 //console.log(API_KEY);
 
 router.get('/', (req, res) => {
-  const category = 'general';
-  const uri = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
+  var category = 'general';
+  var uri = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
 
-  axios.get(uri)
+  axios
+    .get(uri)
     .then(response => {
-      const data = response.data;
-      const articles = data.articles;
-      let articleList = '<ul>';
+      var data = response.data;
+      var articles = data.articles;
+      var articleList = '<ul>';
 
       articles.forEach(article => {
         articleList += `
