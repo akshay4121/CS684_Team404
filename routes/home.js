499fac61e2000cb9a19fa66321d6aa62e757a2c6
cc862 <cc862@njit.edu>
2023-03-12 16:06:44 -0400

    edit on home page to try to work with render take 20

--- routes/home.js
+++ routes/home.js
@@ -13,33 +13,40 @@ const API_KEY = process.env.API_KEY;
 //render debug to see key in log
 //console.log(API_KEY);
 
-router.get("/", async (req, res) => {
-  try {
-    const category = "general";
-    const uri = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
-    const response = await axios.get(uri);
-    const articles = response.data.articles;
-    const articleList = articles.map(article => {
-      return `
-        <li>
-          <img src="${article.urlToImage}" alt="${article.title}" />
-          <h4>${article.title}</h4>
-          <p>${article.description}</p>
-          <a href="${article.url}" target="_blank">Read more</a>
-        </li>
-      `;
-    }).join('');
-    
-    res.render("home", {
-      title: "Home",
-      category: category,
-      articleList: `<ul>${articleList}</ul>`,
-      API_KEY: API_KEY
+router.get('/', (req, res) => {
+  const category = 'general';
+  const uri = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
+
+  axios.get(uri)
+    .then(response => {
+      const data = response.data;
+      const articles = data.articles;
+      let articleList = '<ul>';
+
+      articles.forEach(article => {
+        articleList += `
+          <li>
+            <img src="${article.urlToImage}" alt="${article.title}" />
+            <h2>${article.title}</h2>
+            <p>${article.description}</p>
+            <a href="${article.url}" target="_blank">Read more</a>
+          </li>
+        `;
+      });
+
+      articleList += '</ul>';
+
+      res.render('home', {
+        title: 'Home',
+        category: category,
+        articleList: articleList,
+        API_KEY: API_KEY
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('API call: Internal Server Error');
     });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("API call: Internal Server Error");
-  }
 });
 
 module.exports = router;
