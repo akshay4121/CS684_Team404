d6a7af68891973bab25e082d642a3fcea0449493
cc862 <cc862@njit.edu>
2023-03-28 20:45:20 -0400

    fixed cancel button but and /news/{username} endpoint

--- routes/news.js
+++ routes/news.js
@@ -1,38 +1,71 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const axios = require("axios");
+require('dotenv').config()
+const pool = require("../db/db");
 
-router.get('/', async (req, res) => {
-  var category = "general"; //for landing page user not logged in
-  var API_KEY = "7426a5acd29c4f50b22738d4f8c01f9d";
-  var uri = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
-  
-  axios
-    .get(uri)
-    .then(function (response) {
-      var data = response.data;
-      var articles = data.articles;
-      var articleList = '<ul>';
-      
-      articles.forEach(article => {
-        articleList += `
-          <li>
-            <img src="${article.urlToImage}" alt="${article.title}" />
-            <h2>${article.title}</h2>
-            <p>${article.description}</p>
-            <a href="${article.url}" target="_blank">Read more</a>
-          </li>
-        `;
-      });
-      
-      articleList += '</ul>';
-      
-      res.send(articleList);
-    })
-    .catch(function (error) {
-      res.send(error);
+const API_KEY = process.env.API_KEY;
+
+router.get("/", async (req, res) => {
+  const category = "general";
+
+  try {
+    // Make an API request to the News API with the specified category
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`);
+    const articles = response.data.articles;
+
+    res.status(200).json({ 
+      title: 'News', 
+      category: category,
+      articles: articles 
     });
-    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
+
+router.get("/:username", async (req, res) => {
+  const username = req.params.username;
+  const categories = req.query.category ? [req.query.category.toLowerCase()] : [];
+
+  try {
+    if (categories.length === 0) {
+      // Retrieve the user's preferred categories from the database
+      const result = await pool.query("SELECT General, Business, Entertainment, Health, Science, Sports, Technology FROM Users WHERE Username = ?", [username]);
+      const userPreferences = result && result[0].length > 0 ? {
+        General: result[0][0].General.readInt8(0),
+        Business: result[0][0].Business.readInt8(0),
+        Entertainment: result[0][0].Entertainment.readInt8(0),
+        Health: result[0][0].Health.readInt8(0),
+        Science: result[0][0].Science.readInt8(0),
+        Sports: result[0][0].Sports.readInt8(0),
+        Technology: result[0][0].Technology.readInt8(0),
+      } : null;
+
+      for (let category in userPreferences) {
+        if (userPreferences[category]) {
+          categories.push(category.toLowerCase());
+        }
+      }
+    }
+
+    // Make an API request to the News API with the specified categories
+    const promises = categories.map(category => axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`));
+    const responses = await Promise.all(promises);
+    const articles = responses.flatMap(response => response.data.articles);
+
+    res.status(200).json({ 
+      title: 'News', 
+      categories: categories,
+      articles: articles 
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
 
 module.exports = router;
